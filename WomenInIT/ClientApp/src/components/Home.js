86fbf5abf4fd882f8359244d4e668f11004b767b
isabelladrest <iras@itu.dk>
2022-03-23 14:36:31 +0100

    bedre størrelsesforhold og placeringer på forsiden

--- WomenInIT/ClientApp/src/components/Home.js
+++ WomenInIT/ClientApp/src/components/Home.js
@@ -21,7 +21,6 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <secText className="overskrift">LadyBugs</secText>
         <div className="topDiv">
         <img className="kvindeligProgrammør" src={EmilieChristensen} alt=""/>
          <div className="textAndButton">
@@ -35,9 +34,15 @@ export class Home extends Component {
           <h1 className="overskriftHøjre" id={'section1'}>Hvad er programmering egentlig?</h1>
             <p className="beskrivelseLys"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis gravida nisl, sit amet rutrum turpis elementum tempus. Sed sodales diam vel molestie consequat. Phasellus vitae sollicitudin eros. Sed ut interdum urna. In tincidunt velit vel ornare venenatis. Etiam nec neque non orci consectetur posuere. Nam non suscipit nibh. Donec venenatis nunc sit amet nunc laoreet mattis. Integer commodo orci dolor, eu faucibus diam sagittis et.
             Vivamus congue lorem dui, sit amet tincidunt leo scelerisque sed. Nullam ac semper lorem. Integer vestibulum auctor dui, quis vestibulum nulla vehicula et. Cras ac lectus est. Fusce aliquam accumsan orci, et cursus dui accumsan quis. Donec aliquam quam sit amet arcu semper, quis luctus massa tincidunt. Curabitur sagittis dui quis arcu imperdiet efficitur. Etiam consequat quam a viverra sollicitudin. Cras sit amet elit tellus. Cras quam lacus, porttitor vitae bibendum id, congue nec magna. Fusce in ligula pellentesque, tempor augue ac, bibendum neque. Sed ut leo in libero pulvinar volutpat. Nulla nibh sem, auctor et condimentum non, ultrices id nibh.
-            Aenean ultrices, urna nec dapibus cursus, nisl justo rutrum diam, non aliquet risus nisl rhoncus diam. Nunc sagittis efficitur luctus. Aliquam mi dui, egestas non vestibulum id, sollicitudin non magna. Duis nec dui vel arcu scelerisque posuere at euismod sem. Mauris nisl nunc, lacinia ac nulla at, facilisis pellentesque dolor. Aliquam erat volutpat. Proin a sem nunc.</p>
+            Aenean ultrices, urna nec dapibus cursus, nisl justo rutrum diam, non aliquet risus nisl rhoncus diam. Nunc sagittis efficitur luctus. Aliquam mi dui, egestas non vestibulum id, sollicitudin non magna. Duis nec dui vel arcu scelerisque posuere at euismod sem. Mauris nisl nunc, lacinia ac nulla at, facilisis pellentesque dolor. Aliquam erat volutpat. Proin a sem nunc.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis gravida nisl, sit amet rutrum turpis elementum tempus. Sed sodales diam vel molestie consequat. Phasellus vitae sollicitudin eros. Sed ut interdum urna. In tincidunt velit vel ornare venenatis. Etiam nec neque non orci consectetur posuere. Nam non suscipit nibh. Donec venenatis nunc sit amet nunc laoreet mattis. Integer commodo orci dolor, eu faucibus diam sagittis et.
+            Vivamus congue lorem dui, sit amet tincidunt leo scelerisque sed. Nullam ac semper lorem. Integer vestibulum auctor dui, quis vestibulum nulla vehicula et. Cras ac lectus est. Fusce aliquam accumsan orci, et cursus dui accumsan quis. Donec aliquam quam sit amet arcu semper, quis luctus massa tincidunt. Curabitur sagittis dui quis arcu imperdiet efficitur. Etiam consequat quam a viverra sollicitudin. Cras sit amet elit tellus. Cras quam lacus, porttitor vitae bibendum id, congue nec magna. Fusce in ligula pellentesque, tempor augue ac, bibendum neque. Sed ut leo in libero pulvinar volutpat. Nulla nibh sem, auctor et condimentum non, ultrices id nibh.
+            Aenean ultrices, urna nec dapibus cursus
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis gravida nisl, sit amet rutrum turpis elementum tempus. Sed sodales diam vel molestie consequat. Phasellus vitae sollicitudin eros. Sed ut interdum urna. In tincidunt velit vel ornare venenatis. Etiam nec neque non orci consectetur posuere. Nam non suscipit nibh. Donec venenatis nunc sit amet nunc laoreet mattis. Integer commodo orci dolor, eu faucibus diam sagittis et.
+            Vivamus congue lorem dui, sit amet tincidunt leo scelerisque sed. Nullam ac semper lorem. Integer vestibulum auctor dui, quis vestibulum nulla vehicula et. Cras ac lectus est. Fusce aliquam accumsan orci, et cursus dui accumsan quis. Donec aliquam quam sit amet arcu semper, quis luctus massa tincidunt. Curabitur sagittis dui quis arcu imperdiet efficitur. Etiam consequat quam a viverra sollicitudin. Cras sit amet elit tellus. Cras quam lacus, porttitor vitae bibendum id, congue nec magna. Fusce in ligula pellentesque, tempor augue ac, bibendum neque. Sed ut leo in libero pulvinar volutpat. Nulla nibh sem, auctor et condimentum non, ultrices id nibh.
+            Aenean ultrices, urna nec dapibus cursus</p>
           </div>
-          <Link to="/OmProgrammering" className="læsMereHer">Læs mere her</Link>
+          <Link to="/OmProgrammering" className="omProgrammeringlæsMereHer">Læs mere her</Link>
         </div>
 
         <div className="mellemdiv">
@@ -53,17 +58,19 @@ export class Home extends Component {
             <p className="tal3">3</p>
             <p className ="stepTekst">Til sidst er det bare at gå igang med at programmere. Du kan altid hente hjælp, hvis du går i stå eller bliver i tvivl.</p>
           </div>
+          <Link to="/Opgaver" className="prøvProgrammeringlæsMereHer">Kom igang her</Link>
+
         </div>
 
         <div id="id-of-div" className="historiePink">
         <div className="indhold">
           <div className ="tekstInhold">
-          <h1 className="overskriftHøjre">"Jeg programmerer fordi jeg kan udfolde min kreativitet""</h1>
+          <h1 className="overskriftSuccesForside">"Jeg programmerer fordi jeg kan udfolde min kreativitet""</h1>
           <p className="beskrivelseLys"> Emilie Christensen -20 år.</p>
           </div>
           <img className="billedeSucceshitorie" src={EmilieChristensen} alt=""/>
           </div>
-          <Link to="/SuccesHistorier" className="læsMereHer">Læs mere her</Link>
+          <Link to="/SuccesHistorier" className="omProgrammeringlæsMereHer">Læs mere her</Link>
         </div>
 
        
