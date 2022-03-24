d92356891255c948dab33f6a0ba89feb5e7faa7c
Alberte Kjær Wærens <albw@itu.dk>
2022-03-24 14:17:36 +0100

    tilføjet hints til opgaver

--- WomenInIT/ClientApp/src/components/opgavesider/begynder1.js
+++ WomenInIT/ClientApp/src/components/opgavesider/begynder1.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import './begynder1.css'
+import Collapsible from '../Collapsible.js';
 
 
 export class Begynder1 extends Component {
@@ -12,14 +13,28 @@ export class Begynder1 extends Component {
           <div>
               <div className="baggrundDiv">
               <div className="overskriftdiv" >
-                <secText className="overskrift">Begynder, Opgave 1: Hello world!</secText>
+                <secText className="overskrift">Begynder, Opgave 1: Hello World!</secText>
             </div>
-              <div className="opgaveBeskrivelse">
-                <secText >Den første opgave alle spirende programmøre møde er Hello World!. En opgave der helt simpelt går ud på at få din computer til at udskrive, Hello World. Da dette er er den første opgave så har vi allerede startet klassen op for dig, men hvis du har brug for lidt mere hjælp end det så tryk på et hint nedenfor:</secText>
+              <div className="opgBeskrivelse">
+                <secText >Den første opgave alle spirende programmøre møde er Hello World!. 
+                  En opgave der helt simpelt går ud på at få din computer til at udskrive, Hello World!. 
+                  Da dette er er den første opgave så har vi allerede startet klassen op for dig, 
+                  men hvis du har brug for lidt mere hjælp end det så tryk på et hint nedenfor: 
+                  ...skal måske være noget forklaring eller link til om programmering siden</secText>
               </div>
-   
+              <div className="hints">
+              <Collapsible  label="Hint 1">
+                <p>I denne opgave skal du bruge en kommando der printer en linje ud, også kaldet en string.</p>
+               </Collapsible>
+               <Collapsible  label="Hint 2">
+                <p>Når man printer Strings ud skal der "" rundt om dem </p>
+               </Collapsible>
+               <Collapsible  label="Hint 3">
+                <p>Kommandoen til at printe hvilket som helst ud i terminalen hedder System.out.println()</p>
+               </Collapsible>
+               </div>
             </div>
-            <iframe frameborder="0" width="100%" height="500px" src="https://replit.com/@AlberteWaerens/HelloWorld?lite=true"></iframe>
+              <iframe frameborder="0" width="100%" height="500px" src="https://replit.com/@AlberteWaerens/HelloWorld?lite=true"></iframe>
             </div>
       );
     }
