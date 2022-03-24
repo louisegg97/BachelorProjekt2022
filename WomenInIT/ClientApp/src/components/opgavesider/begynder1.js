074308cf493caf63c39be91b9ea5e288d278e636
Alberte Kjær Wærens <albw@itu.dk>
2022-03-24 10:45:26 +0100

    embeded repl.it

--- WomenInIT/ClientApp/src/components/opgavesider/begynder1.js
+++ WomenInIT/ClientApp/src/components/opgavesider/begynder1.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 
+
 export class Begynder1 extends Component {
     static displayName = Begynder1.name;
   
@@ -13,8 +14,12 @@ export class Begynder1 extends Component {
               <div className="overskriftdiv" >
                 <secText className="overskrift">Begynder, Opgave 1: Hello world!</secText>
             </div>
+              <div className="opgaveBeskrivelse">
+                <secText >Den første opgave alle spirende programmøre møde er Hello World!. En opgave der helt simpelt går ud på at få din computer til at udskrive, Hello World. Da dette er er den første opgave så har vi allerede startet klassen op for dig, men hvis du har brug for lidt mere hjælp end det så tryk på et hint nedenfor:</secText>
+              </div>
+   
             </div>
-            <iframe src="https://trinket.io/embed/java/9a5a4b3e53?showInstructions=true" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+            <iframe frameborder="0" width="100%" height="500px" src="https://replit.com/@AlberteWaerens/HelloWorld?lite=true"></iframe>
             </div>
       );
     }
