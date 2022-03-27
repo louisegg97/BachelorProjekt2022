b90a112633d7296eb71789cd4090c731aa111a30
louisegg97 <79458418+louisegg97@users.noreply.github.com>
2022-03-27 13:12:10 +0200

    nu virker ikonerne

--- WomenInIT/ClientApp/src/components/OmProgrammering.js
+++ WomenInIT/ClientApp/src/components/OmProgrammering.js
@@ -18,30 +18,33 @@ export class OmProgrammering extends Component {
     
     render() {
       return (
-          <div>
+          <div className='body'>
                <h1 className="overskrift_P">Om programmering</h1>
-               <img className="billede_baggrund" src={Baggrund_figur} alt=""/>
-               <Collapsible label="Hvad er programmering egentlig?">
-                    <p>Programmering dækker over udvikling af software og applikationer
-                        m.m. Formålet med software er at udføre nogle opgaver ud fra den 
-                        data, som programmet modtager for at kunne generere et output til
-                        brugeren. For at softwaren ved hvordan opgaverne skal udføres 
-                        anvendes programmeringssprog. Der findes en lang række forskellige 
-                        sprog heraf er de mest udbredte sprog som Java, C#, JavaScript osv. 
-                        Her vil vi give et overblik over de forskellige aspekter, der er ved 
-                        programmering</p>
-               </Collapsible>
+               <div className="billedeBaggrund"></div>
+               <div className='collapsible'>
+                  <Collapsible label="Hvad er programmering egentlig?">
+                        <p>Programmering dækker over udvikling af software og applikationer
+                            m.m. Formålet med software er at udføre nogle opgaver ud fra den 
+                            data, som programmet modtager for at kunne generere et output til
+                            brugeren. For at softwaren ved hvordan opgaverne skal udføres 
+                            anvendes programmeringssprog. Der findes en lang række forskellige 
+                            sprog heraf er de mest udbredte sprog som Java, C#, JavaScript osv. 
+                            Her vil vi give et overblik over de forskellige aspekter, der er ved 
+                            programmering</p>
+                  </Collapsible>
+               </div>
                <div className="icons_">
-                    <div className="grid-container">
-                        <FaColumns size="4em" className="one"/>
-                        <p className="two">Front-end</p>
-                        <FaLaptopCode size="4em" className="three"/>
-                        <p className="four">Back-end</p>
-                        <FaDatabase size="4em" className="five"/>
-                        <p className="six">Databaser</p>
-                    </div>
-                </div>
-               <section className='section1'>
+                  <article>
+                      <section><a href="#frontend"><FaColumns size="4em" className="one"/></a></section>
+                      <section><a href="#frontend"><FaLaptopCode size="4em" className="two"/></a></section>
+                      <section><a href="#backend"><FaDatabase size="4em" className="three"/></a></section>
+                      <section><a href="#backend"><p className="four">Front-end</p></a></section>
+                      <section><a href="#database"><p className="five">Back-end</p></a></section>
+                      <section><a href="#database"><p className="six">Databaser</p></a></section>
+                  </article>
+                 </div>
+
+               <div id="frontend">
                    <h2>Om Frontend</h2>
                    <p>Begrebet front-end dækker over grænsefladen på websteder, 
                        dvs alle de synlige elementer på webstedet eller applikationen. 
@@ -49,7 +52,25 @@ export class OmProgrammering extends Component {
                        fokus på, hvad brugerne ser visuelt. Front-end udviklere koder i HTML, 
                        CSS og Javascript, hvilket er de letteste programmeringssprog at lære. 
                        Disse tre sprog danner fundamentet for webstedet front-end.</p>
-               </section>
+               </div>
+               <div id='backend'>
+                   <h2>Om backend</h2>
+                   <p>Begrebet front-end dækker over grænsefladen på websteder, 
+                       dvs alle de synlige elementer på webstedet eller applikationen. 
+                       Der er således fokus på webudvikllingen af klientsiden, da der er 
+                       fokus på, hvad brugerne ser visuelt. Front-end udviklere koder i HTML, 
+                       CSS og Javascript, hvilket er de letteste programmeringssprog at lære. 
+                       Disse tre sprog danner fundamentet for webstedet front-end.</p>
+               </div>
+               <div id="database">
+                   <h2>Om databaser</h2>
+                   <p>Begrebet front-end dækker over grænsefladen på websteder, 
+                       dvs alle de synlige elementer på webstedet eller applikationen. 
+                       Der er således fokus på webudvikllingen af klientsiden, da der er 
+                       fokus på, hvad brugerne ser visuelt. Front-end udviklere koder i HTML, 
+                       CSS og Javascript, hvilket er de letteste programmeringssprog at lære. 
+                       Disse tre sprog danner fundamentet for webstedet front-end.</p>
+               </div>
           </div>
       );
     }
