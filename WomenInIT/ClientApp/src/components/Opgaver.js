e49d3a66d75f47de07f2856fea23a10ee24a1124
isabelladrest <iras@itu.dk>
2022-04-29 13:19:05 +0200

    Tilføjet tekst til opgave side

--- WomenInIT/ClientApp/src/components/Opgaver.js
+++ WomenInIT/ClientApp/src/components/Opgaver.js
@@ -23,10 +23,15 @@ export class Opgaver extends Component {
             <div className="overskriftdiv" id="baggrund">
               <secText className="overskriftOpgaver" >Opgaver</secText>
             <p className="intro">
-            Her har du mulighed for at prøve at programmer selv. 
-            Der er opgaver for dig der er begynder, let øvet og for dig der er øvet. 
-            Uanset niveau kan du prøve kræfter med programmering i frontend, backend og databaser 
-            - hvis du vil vide mere om disse kan du læse om dem her
+            Her har du mulighed for at prøve kræfter med at programmere selv. 
+            Vi mener at det er vigtigt at opleve programmering selv, for at forstå hvordan det virker, og hvad det kan. 
+            Vi har derfor lavet nogle opgaver, som du kan kaste dig ud i, uanset din erfaring med programmering. 
+            Du har muligheder for at vælge opgaver indenfor Frontend, backend og database, alt efter hvad ud har mest lyst til at prøve. 
+            Hvis du er i tvivl om hvad det er, eller ønsker at vide mere kan du læse om det i “Om programmering”. 
+            For hver kategori er der opgaver til dig der er begynder, til dig der er let øvet og til dig der er øvet i programmering. 
+            Der er tre opgaver til dem alle, og du kan helt frit vælge hvilke du vil løse. Hvis du sidder fast, og ikke er sikker på hvordan du skal løse opgaven, er der tre hints du kan se, som kan hjælpe dig lidt på vej.
+            <br />
+            God fornøjelse!
             </p>
             <div className="gifdiv" >
             <img className="gif" src={gif} alt=""/>
